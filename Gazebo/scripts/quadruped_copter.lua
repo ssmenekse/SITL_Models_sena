e51a5810d1094c75acbb44fc0a6192b45e1be16a
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-03-16 17:41:57 +0000

    Hexapod Copter: clean up model, script and params for quadruped_copter
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- Gazebo/scripts/quadruped_copter.lua
+++ Gazebo/scripts/quadruped_copter.lua
@@ -22,39 +22,7 @@
 --
 -- CAUTION: This script should only be used with ArduPilot firmware
 
-
-local FRAME_LEN = 177 -- frame length in mm
-local FRAME_WIDTH = 101 -- frame width in mm
-
-local COXA_LEN = 28.5 -- distance (in mm) from coxa (aka hip) servo to femur servo
-local FEMUR_LEN = 76.2 -- distance (in mm) from femur servo to tibia servo
-local TIBIA_LEN = 102 -- distance (in mm) from tibia servo to foot
-
---body position and rotation parameters
-local body_rot_max = 10 -- body rotation maximum for any individual axis
-local body_rot_x = 0 -- body rotation about the X axis (i.e. roll rotation)
-local body_rot_y = 0 -- body rotation about the Y axis (i.e. pitch rotation)
-local body_rot_z = 0 -- body rotation about the Z axis (i.e. yaw rotation)
-local body_pos_x = 0 -- body position in the X axis (i.e. forward, back).  should be -40mm to +40mm
-local body_pos_y = 0 -- body position in the Y axis (i.e. right, left).  should be -40mm to +40mm
-local body_pos_z = 0 -- body position in the Z axis (i.e. up, down).  should be -40mm to +40mm
-
--- servo angles when robot is disarmed and resting body on the ground
-local disarmed_leg_angles = {
-  45, -90, 40,  -- front right leg (coxa, femur, tibia)
-  -45, -90, 40, -- front left leg (coxa, femur, tibia)
-  -45, -90, 40, -- back left leg (coxa, femur, tibia)
-  45, -90, 40   -- back right leg (coxa, femur, tibia)
-}
-
-local armed_leg_angles = {
-  0, -25, -60,  -- front right leg (coxa, femur, tibia)
-  0, -25, -60, -- front left leg (coxa, femur, tibia)
-  0, -25, -60, -- back left leg (coxa, femur, tibia)
-  0, -25, -60   -- back right leg (coxa, femur, tibia)
-}
-
--- Adapted rom https://github.com/ArduPilot/ardupilot/blob/master/libraries/AP_Scripting/examples/MotorMatrix_setup.lua
+-- Adapted from https://github.com/ArduPilot/ardupilot/blob/master/libraries/AP_Scripting/examples/MotorMatrix_setup.lua
 
 -- duplicate the standard + Quad mix
 MotorsMatrix:add_motor_raw(0,-1, 0, 1, 2)
@@ -64,7 +32,22 @@ MotorsMatrix:add_motor_raw(3, 0,-1,-1, 3)
 
 assert(MotorsMatrix:init(4), "Failed to init MotorsMatrix")
 
-motors:set_frame_string("Set Quadruped Copter MotorMatrix")
+motors:set_frame_string("Quadruped Copter")
+
+-- servo angles when robot is disarmed and resting body on the ground
+local disarmed_leg_angles = {
+   45, -90, 40, -- front right leg (coxa, femur, tibia)
+  -45, -90, 40, -- front left leg (coxa, femur, tibia)
+  -45, -90, 40, -- back left leg (coxa, femur, tibia)
+   45, -90, 40  -- back right leg (coxa, femur, tibia)
+}
+
+local armed_leg_angles = {
+  0, -25, -60, -- front right leg (coxa, femur, tibia)
+  0, -25, -60, -- front left leg (coxa, femur, tibia)
+  0, -25, -60, -- back left leg (coxa, femur, tibia)
+  0, -25, -60  -- back right leg (coxa, femur, tibia)
+}
 
 function update()
 
@@ -82,13 +65,14 @@ function update()
     leg_angles = disarmed_leg_angles
   end
 
-  -- legs 1 - 12 (first leg output is 4 as 0 - 3 are assigned to motors)
   for i = 1, 12 do
-    -- norm_angle in [-1, 1]
+    -- normalised angle is in [-1, 1]
     local norm_angle = leg_angles[i] * leg_servo_direction[i] / 90
-    -- pwm in [1000, 2000]
+    -- ensure pwm is in [1000, 2000]
     local pwm = math.floor(norm_angle * 500 + 1500)
 
+    -- Q: is index for assigned channel or absolute?
+    -- A: it is absolute (so offset for the 4 motors)
     SRV_Channels:set_output_pwm_chan_timeout(i + 3, pwm, 1000)
   end
 
